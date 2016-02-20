c6995a8213c41ab14f6b1cb3487da84d2ec1d336
N32N <nicolas.bourdin@etudiant.mines-nantes.fr>
2016-02-20 15:14:35 +0100

    -

--- src/Solution/Planning.java
+++ src/Solution/Planning.java
@@ -54,7 +54,7 @@ public class Planning {
                 time = 0;
             } else {
                 planning[instance.getNbM1() + machine - 1][line][0] = currentSecond.getNumber();
-                int release = this.getRelease(currentSecond.getNumber());//Date à partir de laquelle le job est produit(fin) au premier étage
+                int release = this.readPlanning(currentSecond.getNumber(), 1, false);//Date à partir de laquelle le job est produit(fin) au premier étage
                 if (time < release) time = release;//Vérification de la release à l'étage précédent
                 planning[instance.getNbM1() + machine - 1][line][1] = time;
                 time += currentSecond.getQuantity() * instance.getDuration(machine - 1, currentSecond.getType()); //ajout processTime de currentSecond sur machine
@@ -72,15 +72,18 @@ public class Planning {
         }
     }
 
-    public int getRelease(int jobNumber) {
-        for (int indexMachine = 0; indexMachine < instance.getNbM1(); indexMachine++) {
+    public int readPlanning(int jobNumber, int stage, boolean start) {
+        stage--;
+        for (int indexMachine = stage * instance.getNbM1(); indexMachine < instance.getNbM1() + stage * instance.getNbM2(); indexMachine++) {
             int indexJob = 0;
-            while (indexJob < planning[indexMachine].length && planning[indexMachine][indexJob][0] != 0) {
-                if (jobNumber == planning[indexMachine][indexJob][0]) return planning[indexMachine][indexJob][2];
+            while (indexJob < planning[indexMachine].length && planning[indexMachine][indexJob][0] > 0) {
+                if (jobNumber == planning[indexMachine][indexJob][0])
+                    if (start) return planning[indexMachine][indexJob][1];
+                    else return planning[indexMachine][indexJob][2];
                 indexJob++;
             }
         }
-        System.out.println("job " + jobNumber + " is not scheduled at first floor but at second");
+        System.out.println("job " + jobNumber + " presents a problem");
         return 0;
     }
 
@@ -113,29 +116,11 @@ public class Planning {
      * @return
      */
     public boolean isValid() {
-        return /*checkStock() &&*/ allJobOk();
+        return checkStock() && allJobOk();
     }
 
     public int objective() {
         return instance.getCoutProd() * fin + instance.getCoutPenalite() * lateTime;
-    }
-
-    public int getDelivery(int jobNumber) {
-        boolean found = false;
-        int machine = this.instance.getNbM1();
-        int j = 0;
-        while (!found && machine < planning.length) {
-            j = 0;
-            while (!found && j < planning[machine].length && planning[machine][j][0] > 0) {
-                if (planning[machine][j][0] == jobNumber) found = true;
-                else j++;
-            }
-            machine++;
-        }
-        if (!found) {
-            System.out.println("Job " + jobNumber + " is not scheduled and has no delivery time");
-            return 0;
-        } else return planning[machine][j][2];
     }
 
     /**
@@ -171,14 +156,30 @@ public class Planning {
         return true;
     }
 
+    public boolean[] needStock() {
+        boolean[] needStock = new boolean[instance.getNbJob()];
+        for (int m = 0; m < instance.getNbM1(); m++) {
+            int line = 0;
+            while (line < planning[m].length && planning[m][line][0] > 0) {
+                if (planning[m][line][2] == readPlanning(planning[m][line][0], 2, true))
+                    needStock[planning[m][line][0] - 1] = false;
+                else needStock[planning[m][line][0] - 1] = true;
+                line++;
+            }
+        }
+        return needStock;
+    }
+
     /**
      * @return true if no more than 3 jobs (with quantities lower than the capacities) are wainting between 1 machine at 2nd floor
      */
     public boolean checkStock() {
+        boolean[] needStock = needStock();
         //Prepare stocks for every machine at second floor
         Stock[] stocks = new Stock[instance.getNbM2()];
         for (int s = 0; s < stocks.length; s++)
             stocks[s] = new Stock(instance, instance.getStockCapa(s));
+
         // cursors
         int[] indexMachine = new int[planning.length];
         for (int i = 0; i < indexMachine.length; i++)
@@ -190,18 +191,14 @@ public class Planning {
             int job = 0;
             int min = fin;
             for (int i = 0; i < indexMachine.length; i++)
-                if (date(i, indexMachine[i]) <= min) {
-                    min = date(i, indexMachine[i]);
-                    machine = i;
-                    job = indexMachine[i];
-                }
-            // change the stocks according to the current jobs finishing at 1st floor or starting at 2nd floor
-            if (planning[machine][job][0] > 0)
-                if (machine < instance.getNbM1()) {
-                    stocks[machine].add(instance.getJob(planning[machine][job][0] - 1).getQuantity());
-                    if (!stocks[machine].isValid()) return false;
-                } else
-                    stocks[machine - instance.getNbM1()].remove(-instance.getJob(planning[machine][job][0] - 1).getQuantity());
+                if (indexMachine[i] < planning[i].length && date(i, indexMachine[i]) <= min){
+                        min = date(i, indexMachine[i]);
+                        machine = i;
+                        job = indexMachine[i];
+                    }
+
+            if (planning[machine][job][0] > 0 && needStock[planning[machine][job][0] - 1])
+                if (!stockUpdate(stocks, machine, job)) return false;
 
             indexMachine[machine]++;
             end = true;
@@ -212,7 +209,29 @@ public class Planning {
         return true;
     }
 
+    /**
+     * Change the stocks according to the current job finishing at 1st floor or starting at 2nd floor
+     *
+     * @param stocks
+     * @param machine
+     * @param job
+     */
+    public boolean stockUpdate(Stock[] stocks, int machine, int job) {
+        if (machine < instance.getNbM1()) {
+            stocks[machine].add(instance.getJob(planning[machine][job][0] - 1).getQuantity());
+            if (!stocks[machine].isValid()) return false;
+        } else
+            stocks[machine - instance.getNbM1()].remove(-instance.getJob(planning[machine][job][0] - 1).getQuantity());
+        return true;
+    }
+
+    /**
+     * @param machine
+     * @param line
+     * @return the date (release if first floor, start if second floor) the the job at line "line" on machine "machine"
+     */
     public int date(int machine, int line) {
+        if (planning[machine][line][0] == 0) return Integer.MAX_VALUE;
         if (machine < instance.getNbM1()) return planning[machine][line][2];
         else return planning[machine][line][1];
     }
--- src/Tests/PlanningTest.java
+++ src/Tests/PlanningTest.java
@@ -1,4 +1,5 @@
 package Tests;
+
 import Resolution.Solver;
 import Solution.*;
 import Instance.Instance;
@@ -16,7 +17,37 @@ public class PlanningTest {
         Planning p = new Planning(in, sol);
 
         sol.print();
-        System.out.printf("allJobOk : "+p.allJobOk());
-        //sol.printPlanning();
+        System.out.println("allJobOk : "+p.allJobOk());
+        System.out.println("checkStock : " + p.checkStock());
+        sol.printPlanning();
+        //testDate(p);
+        //testNeedStock(p);
+        //testStockUpdate(p, in);
+
+    }
+
+    public static void testStockUpdate(Planning p, Instance instance){
+        Stock[] stocks = new Stock[instance.getNbM2()];
+        for (int s = 0; s < stocks.length; s++)
+            stocks[s] = new Stock(instance, instance.getStockCapa(s));
+        System.out.println(""+p.stockUpdate(stocks, 0, 0));
+        System.out.println(""+p.stockUpdate(stocks, 1, 0));
+    }
+
+    public static void testNeedStock(Planning p){
+        boolean[] n = p.needStock();
+        for(int i = 0; i<n.length;i++) System.out.println("job "+(i+1)+" - "+n[i]);
+    }
+
+    public static void testDate(Planning p) {
+        System.out.println("FIRST");
+        System.out.println("1 (32) : " + p.date(0, 0));
+        System.out.println("3 (133) : " + p.date(0, 4));
+        System.out.println("6 (113) : " + p.date(0, 3));
+
+        System.out.println("SECOND");
+        System.out.println("1 (32) : " + p.date(1, 0));
+        System.out.println("3 (133) : " + p.date(2, 1));
+        System.out.println("6 (152) : " + p.date(3, 1));
     }
 }
